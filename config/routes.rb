a0cbed6c76752ca784015b43a8286bb512712f46
gorotakeda <rugbytkg3@gmail.com>
2021-09-20 15:27:01 +0900

    投稿削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   controllers: { registrations: 'registrations' }
   root "posts#index"
   get '/users/:id', to: 'users#show', as: 'user'
-  resources :posts, only: %i(index new create show) do
+  resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
   end
 end
